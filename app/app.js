aaca168010d22b0fb1c38e8f05580750d7457e37
igor <i.golod@andersenlab.com>
2016-12-20 11:11:06 +0200

    add ui-route , but not correct work

--- app/app.js
+++ app/app.js
@@ -1,2 +1,18 @@
 var angular = require('angular');
-var myApp = angular.module('app',[]);
+require("angular-ui-router");
+var app = angular.module('app',['ui.router']);
+var angularUiRouter = require("angular-ui-router");
+
+app.controller('initCtrl', ['$scope', function($scope) {
+    $scope.greeting = 'Hello,';
+}]);
+
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('about', {
+            url: "/about",
+            templateUrl: 'about.html'
+        });
+    
+});
